eb709b41f45d5094cebbdf3bfddee516deb97f8b
YanZisuka <hanndrednine@gmail.com>
2022-07-27 22:35:04 +0900

    :bug: [FIX] App.tsx 경로
    import 경로 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import Modal from "components/commons/Modal";
-import ProfileView from "components/ProfileView";
+import ProfileView from "pages/ProfileView";
 
 function App() {
   return (
