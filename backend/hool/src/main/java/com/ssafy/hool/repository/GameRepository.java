595b85c66f7401bceacb4e595f1d14a2dcd3234b
hanwool <wjsgksdnf11@naver.com>
2022-07-27 09:22:44 +0900

    :coffin: [REFACTOR] GameRepository 코드 수정
    GameRepository 중 불필요 코드 삭제

--- backend/hool/src/main/java/com/ssafy/hool/repository/GameRepository.java
+++ backend/hool/src/main/java/com/ssafy/hool/repository/GameRepository.java
@@ -10,8 +10,5 @@ import javax.persistence.EntityManager;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
-
-//    public Game_history findByMemberId(Long memberId);
-    public Game findByConferenceId(Long conferenceId);
 }
 
