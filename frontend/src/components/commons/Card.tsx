92990b0a59aba95728f1569aec95e7e065da3449
SSAFY <ejshin0213@naver.com>
2022-07-27 16:24:27 +0900

    :lipstick: [DESIGN] Card 컴포넌트 디자인 수정
    Card 컴포넌트 디자인 수정

--- frontend/src/components/commons/Card.tsx
+++ frontend/src/components/commons/Card.tsx
@@ -1,12 +1,13 @@
 import React, { ReactNode } from "react";
 import styled from "styled-components";
-import { darkTheme } from "@/styles/Theme";
+import { darkTheme } from "styles/Theme";
 
 const CardDIV = styled.div`
   margin: 2rem auto;
   background: ${(props) => props.bgColor || darkTheme.adaptiveGrey700};
   border: 2px solid ${darkTheme.adaptiveGrey700};
   border-radius: 4px;
+  border: 1px;
 `;
 type PropsType = {
   background: string;
