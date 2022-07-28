4bbd454f83d40113a2dd851a531148921db448d5
HojinAn <asdv94@naver.com>
2022-07-28 12:23:25 +0900

    :lipstick: [DESIGN] NavHeader 디자인 수정

--- frontend/src/components/commons/nav/NavHeader.tsx
+++ frontend/src/components/commons/nav/NavHeader.tsx
@@ -2,20 +2,20 @@ import profileDefaultImg from "assets/profile-default-img.png";
 import styled from "styled-components";
 import { darkTheme } from "styles/Theme";
 
-const { adaptiveGrey200, adaptiveGrey800 } = darkTheme;
+const { adaptiveGrey200, mainColor } = darkTheme;
 
 const NavHeader = () => {
   return (
     <Header>
-      <Icon className="fa-solid fa-bell" />
       <NavUser>
         <ProfileImg
           src={profileDefaultImg}
           alt={`${profileDefaultImg}의 프로필 이미지`}
         />
-        <p>Andrew</p>
-        <select className="select"></select>
+        <ProfileName>Andrew</ProfileName>
+        <Icon className="fa-solid fa-chevron-down" />
       </NavUser>
+      <Icon className="fa-solid fa-bell" />
     </Header>
   );
 };
@@ -25,28 +25,30 @@ const Header = styled.nav`
   margin: 0 auto;
   left: 0;
   right: 0;
-  padding: 1rem;
-  width: 94vw;
-  height: 2.5rem;
+  padding: 1rem 4rem;
+  height: 2rem;
   display: flex;
   justify-content: end;
   align-items: center;
-  background-color: ${adaptiveGrey800};
+  background-color: ${mainColor};
 `;
 const Icon = styled.i`
-  margin-right: 1rem;
   color: ${adaptiveGrey200};
 `;
 const ProfileImg = styled.img`
   height: 2rem;
   border-radius: 4px;
 `;
+const ProfileName = styled.p`
+  color: ${adaptiveGrey200};
+  font-size: 1rem;
+`;
 const NavUser = styled.div`
+  margin-right: 1rem;
   width: 10rem;
   display: flex;
   align-items: center;
   justify-content: space-evenly;
-  color: ${adaptiveGrey200};
 `;
 
 export default NavHeader;
