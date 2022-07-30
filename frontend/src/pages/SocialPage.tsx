70f42cbec1d95de7c043148be89ba7d27fe52ec2
YanZisuka <hanndrednine@gmail.com>
2022-07-30 10:05:12 +0900

    :recycle: [REFACTOR] 소셜 페이지 구조화
    Header & Content 분리

--- frontend/src/pages/SocialPage.tsx
+++ frontend/src/pages/SocialPage.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 import styled from "styled-components";
 
-import Social from "components/social/Social";
+import SocialHeader from "components/social/SocialHeader";
+import MyFriends from "components/social/MyFriends";
+import Requests from "components/social/Requests";
 
 function SocialView() {
+  const [isDisplayMyFriends, setIsDisplayMyFriends] = useState(true);
+
+  const myFriends = [
+    { nickname: "Thomas", curPos: "일본 vs 대한민국 같이봐요", status: "live" },
+    { nickname: "Bell", curPos: "접속중", status: "loggedIn" },
+    { nickname: "Dijkstra", curPos: "12 minutes ago", status: "loggedOut" },
+  ];
+
+  const requests = [{ nickname: "Ronaldo", msg: "친해지고싶어요." }];
+
+  const searchRes = [{ nickname: "Kruskal", email: "mst@gmail.com" }];
+
   return (
     <Container>
-      <Social />
+      <SocialHeader
+        isDisplayMyFriends={isDisplayMyFriends}
+        setIsDisplayMyFriends={setIsDisplayMyFriends}
+      />
+      {isDisplayMyFriends && <MyFriends />}
+      {!isDisplayMyFriends && <Requests />}
     </Container>
   );
 }
