57d53491ce92948902fbec27e212670cbe303067
SSAFY <ejshin0213@naver.com>
2022-07-30 18:25:51 +0900

    :sparkles: [FEAT] ErrorPage 및 Error 컴포넌트 생성
    ErrorPage , Error 컴포넌트 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { Layout } from "components/layouts/Layout";
 import { Routes, Route } from "react-router-dom";
-import { HomeView, ProfileView } from "pages";
+import { HomeView, ProfileView, ErrorPage, LoginPage } from "pages";
 
 function App() {
   return (
@@ -8,6 +8,13 @@ function App() {
       <Routes>
         <Route path="/" element={<HomeView />} />
         <Route path="/profile" element={<ProfileView />} />
+        <Route
+          path="/error"
+          element={
+            <ErrorPage errorMessage="(Error Message : 주소 입력 오류)" /> //Router Page props주는 방법 학습해야함.
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Layout>
   );
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,2 +1,4 @@
 export {default as HomeView} from './HomeView'
 export {default as ProfileView} from './ProfileView'
+export {default as ErrorPage} from './ErrorPage'
+export {default as LoginPage} from './LoginPage'
