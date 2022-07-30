3489c8b62b360a59f41e18cccfc1fa0af490877f
HojinAn <asdv94@naver.com>
2022-07-30 20:30:59 +0900

    :truck: [RENAME] state -> store로 변경
    state/store.ts 대신 store/index.ts를 사용해서 의도에 더 맞도록 수정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import App from "App";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
-import App from "App";
+import { Provider } from "react-redux";
 
 import { GlobalStyle } from "./styles/GlobalStyle";
+import { store } from "store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
