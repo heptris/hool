a5c719978729792e00ccbbf40dbf9704d67e01e6
HojinAn <asdv94@naver.com>
2022-08-01 01:27:20 +0900

    :lipstick: [DESIGN] 미팅룸 리스트 UI 구현 (S07P12A408-8)

--- frontend/src/components/meeting/MeetingList.tsx
+++ frontend/src/components/meeting/MeetingList.tsx
@@ -1,18 +1,57 @@
 import styled from "styled-components";
 
+import Button from "components/commons/Button";
 import MeetingListItem from "./MeetingListItem";
+import Modal from "components/commons/Modal";
 
 const MeetingList = () => {
   return (
-    <ItemList>
-      <MeetingListItem />
-      <MeetingListItem />
-      <MeetingListItem />
-    </ItemList>
+    <Wrapper>
+      <Container>
+        <Header>
+          <Button
+            height={3}
+            width={7}
+            marginTop={3}
+            marginBottom={3}
+            CSSProps={{ float: "right" }}
+            text={"응원방 만들기"}
+          />
+        </Header>
+        <ItemList>
+          <MeetingListItem />
+          <MeetingListItem />
+          <MeetingListItem />
+          <MeetingListItem />
+          <MeetingListItem />
+          <MeetingListItem />
+          <MeetingListItem />
+          <MeetingListItem />
+          <MeetingListItem />
+        </ItemList>
+      </Container>
+    </Wrapper>
   );
 };
-const ItemList = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, 18rem);
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
 `;
+const Container = styled.section`
+  width: 90%;
+`;
+const Header = styled.header`
+  overflow: hidden;
+`;
+const ItemList = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 20rem);
+  grid-gap: 2rem;
+  padding-top: 5rem;
+  justify-content: center;
+  margin: auto;
+`;
+const MeetingListModal = styled(Modal)``;
 export default MeetingList;
