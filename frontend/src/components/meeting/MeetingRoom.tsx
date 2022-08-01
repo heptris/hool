ae9b3e4d5e7b12c0cf170ee6ccb2c96124a942c3
SSAFY <ejshin0213@naver.com>
2022-08-01 17:21:25 +0900

    :lipstick: [DESIGN] MeetingRoom 컴포넌트 스타일 변경
    MeetingRoom 컴포넌트 스타일 변경

--- frontend/src/components/meeting/MeetingRoom.tsx
+++ frontend/src/components/meeting/MeetingRoom.tsx
@@ -1,35 +1,49 @@
 import styled from "styled-components";
 import { darkTheme } from "styles/Theme";
+import MeetingMessage from "./MeetingMessage";
 
-import Room from "./Room";
+import MeetingVideo from "./MeetingVideo";
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
 `;
 
+const FlexBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
 const MeetingBox = styled.div`
-  width: 91.68rem;
+  width: 66.375rem;
   height: 57.68rem;
   margin-top: 2.5rem;
   margin-left: 3rem;
   display: grid;
-  padding: 1rem;
+  flex-shrink: 3;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  border-radius: 4px;
+  padding-left: 1rem;
   grid-template-rows: repeat(2, 21rem);
-  grid-template-columns: repeat(2, 45rem);
-  grid-auto-rows: 100px;
-  background-color: ${darkTheme.bgColor};
+  grid-template-columns: repeat(2, 33rem);
+  background-color: ${darkTheme.mainColor};
 `;
 
 const MeetingRoom = () => {
   return (
     <Container>
-      <MeetingBox>
-        <Room />
-        <Room />
-        <Room />
-        <Room />
-      </MeetingBox>
+      <FlexBox>
+        <MeetingBox>
+          <MeetingVideo />
+          <MeetingVideo />
+          <MeetingVideo />
+          <MeetingVideo />
+        </MeetingBox>
+        <MeetingMessage />
+      </FlexBox>
     </Container>
   );
 };
