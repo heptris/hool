137a0563f1af12a369db515335507e08530b526a
HAN SEUNG JAE <hanndrednine@gmail.com>
2022-08-03 22:23:17 +0900

    :rocket: [CHORE] nginx + react 배포 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
-import dns from "dns"
+// import dns from "dns"
 
-dns.setDefaultResultOrder('verbatim')
+// dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    fs: {
+      strict: false,
+    },
+  },
   plugins: [svgr(), react(), tsconfigPaths()],
 });
