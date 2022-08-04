983703fea212e14c7b5fccbb9b4322ec1cd5c14d
hanwool <wjsgksdnf11@naver.com>
2022-08-04 20:52:30 +0900

    :sparkles: [FEAT] querydsl 기능 추가
    querydsl 설정 추가

--- backend/hool/src/test/java/com/ssafy/hool/HoolApplicationTests.java
+++ backend/hool/src/test/java/com/ssafy/hool/HoolApplicationTests.java
@@ -1,10 +1,20 @@
 package com.ssafy.hool;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.hool.domain.member.QMember;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @SpringBootTest
+@Transactional
 class HoolApplicationTests {
+
 
 	@Test
 	void contextLoads() {
