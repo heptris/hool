4605b1013765c881a231ce6ebcc0fbabba80d39a
HojinAn <asdv94@naver.com>
2022-08-05 18:53:59 +0900

    :zap: [REFACTOR] pathname에 따른 dispatch 발생 개선
    navMode에 따라 레이아웃 변경되도록 수정

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,55 +1,26 @@
-import { useCallback, useEffect } from "react";
-import { useRouter } from "@tanstack/react-location";
+import { useEffect } from "react";
+import { useLocation } from "@tanstack/react-location";
 import { useDispatch, useSelector } from "react-redux";
 
-import { RootState, setIsNavbar, setIsMeetingNavbar } from "store";
+import { RootState, setNavMode } from "store";
 
 import { Layout } from "components/layouts/Layout";
-import { MeetingModal } from "components/meeting";
-import MeetingGameModal from "components/meeting/gameModal/MeetingGameModal";
 
 export default function App({ children }: { children: React.ReactNode }) {
+  const { navMode } = useSelector((state: RootState) => state.navbar);
   const dispatch = useDispatch();
-  const router = useRouter();
+  const location = useLocation();
 
   useEffect(() => {
-    console.log(router.state.location.pathname);
-    if (
-      router.state.location.pathname === "/auth/login" ||
-      router.state.location.pathname === "/auth/signup" ||
-      router.state.location.pathname === "/auth/find" ||
-      router.state.location.pathname === "/error"
-    ) {
-      dispatch(setIsNavbar(false));
-      dispatch(setIsMeetingNavbar(false));
-    } else if (router.state.location.pathname === "/meeting") {
-      dispatch(setIsMeetingNavbar(true));
-      dispatch(setIsNavbar(false));
-      console.log("dispatch");
-    } else {
-      dispatch(setIsNavbar(true));
-      dispatch(setIsMeetingNavbar(false));
-    }
-  });
+    location.current.pathname.slice(0, 6) === `/auth/` ||
+    location.current.pathname === "/error"
+      ? dispatch(setNavMode("unseen"))
+      : location.current.pathname.slice(0, 9) === "/meeting/"
+      ? dispatch(setNavMode("meetingRoom"))
+      : dispatch(setNavMode("default"));
+  }, [location.current.pathname]);
 
-  const { isNavbar, isCreatingRoom, isCreatingGame, isMeetingNavbar } =
-    useSelector((state: RootState) => {
-      console.log("selector");
-      return state.navbar;
-    });
-
-  console.log("isNavbar:" + isNavbar + ", IsMeetingNavbar: " + isMeetingNavbar);
   return (
-    <>
-      {isNavbar || isMeetingNavbar ? (
-        <Layout>
-          {children}
-          {isCreatingRoom && <MeetingModal />}
-          {isCreatingGame && <MeetingGameModal />}
-        </Layout>
-      ) : (
-        <>{children}</>
-      )}
-    </>
+    <>{navMode !== "unseen" ? <Layout>{children}</Layout> : <>{children}</>}</>
   );
 }
