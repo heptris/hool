9392dfa7a6d83900ea40f8449c5283c3516ddf8e
SSAFY <ejshin0213@naver.com>
2022-08-05 16:23:28 +0900

    :construction: [ING] NavSide meeting room 상태관리 개발 중
    meeting room 상태관리 개발 중

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useRouter } from "@tanstack/react-location";
 import { useDispatch, useSelector } from "react-redux";
 
-import { RootState, setIsNavbar } from "store";
+import { RootState, setIsNavbar, setIsMeetingNavbar } from "store";
 
 import { Layout } from "components/layouts/Layout";
 import { MeetingModal } from "components/meeting";
@@ -11,27 +11,41 @@ import MeetingGameModal from "components/meeting/gameModal/MeetingGameModal";
 export default function App({ children }: { children: React.ReactNode }) {
   const dispatch = useDispatch();
   const router = useRouter();
+
   useEffect(() => {
     console.log(router.state.location.pathname);
-    router.state.location.pathname === "/auth/login" ||
-    router.state.location.pathname === "/auth/signup" ||
-    router.state.location.pathname === "/auth/find" ||
-    router.state.location.pathname === "/error"
-      ? dispatch(setIsNavbar(false))
-      : dispatch(setIsNavbar(true));
+    if (
+      router.state.location.pathname === "/auth/login" ||
+      router.state.location.pathname === "/auth/signup" ||
+      router.state.location.pathname === "/auth/find" ||
+      router.state.location.pathname === "/error"
+    ) {
+      dispatch(setIsNavbar(false));
+      dispatch(setIsMeetingNavbar(false));
+    } else if (router.state.location.pathname === "/meeting") {
+      dispatch(setIsMeetingNavbar(true));
+      dispatch(setIsNavbar(false));
+      console.log("dispatch");
+    } else {
+      dispatch(setIsNavbar(true));
+      dispatch(setIsMeetingNavbar(false));
+    }
   });
 
-  const { isNavbar, isCreatingRoom, isCreatingGame } = useSelector(
-    (state: RootState) => state.navbar
-  );
+  const { isNavbar, isCreatingRoom, isCreatingGame, isMeetingNavbar } =
+    useSelector((state: RootState) => {
+      console.log("selector");
+      return state.navbar;
+    });
 
+  console.log("isNavbar:" + isNavbar + ", IsMeetingNavbar: " + isMeetingNavbar);
   return (
     <>
-      {isNavbar ? (
+      {isNavbar || isMeetingNavbar ? (
         <Layout>
           {children}
           {isCreatingRoom && <MeetingModal />}
-          {isCreatingGame && <MeetingGameModal/>}
+          {isCreatingGame && <MeetingGameModal />}
         </Layout>
       ) : (
         <>{children}</>
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,6 +1,7 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 interface StoreProps {
   isNavbar?: boolean;
+  isMeetingNavbar?: boolean;
 }
 const navbar = createSlice({
   name: "navbar",
@@ -8,6 +9,7 @@ const navbar = createSlice({
     isNavbar: true,
     isCreatingRoom: false,
     isCreatingGame: false,
+    isMeetingNavbar: false,
   },
   reducers: {
     setIsNavbar(state, actions) {
@@ -18,7 +20,11 @@ const navbar = createSlice({
     },
     setIsCreatingGame(state, actions) {
       state.isCreatingGame = actions.payload;
+    },
+    setIsMeetingNavbar(state, actions) {
+      state.isMeetingNavBar = actions.payload;
     }
+
   },
 });
 export const store = configureStore({
@@ -26,7 +32,7 @@ export const store = configureStore({
     navbar: navbar.reducer,
   },
 });
-export const { setIsNavbar, setIsCreatingRoom, setIsCreatingGame } = navbar.actions;
+export const { setIsNavbar, setIsCreatingRoom, setIsCreatingGame, setIsMeetingNavbar } = navbar.actions;
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
