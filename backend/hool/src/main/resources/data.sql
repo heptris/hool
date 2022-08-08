3803fe92c6eaeb0ca85becee40f3690fd87d5cfb
hanwool <wjsgksdnf11@naver.com>
2022-08-08 14:47:49 +0900

    :white_check_mark: [TEST] 더미데이터 코드 수정
    더미데이터 코드 수정

--- backend/hool/src/main/resources/data.sql
+++ backend/hool/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 insert into member (created_date, last_modified_date, authority, member_email, member_status, name, nick_name, password, point)
-values (now(), now(), 'ROLE_USER', 'aa@naver.com', 'OFFLINE', 'aa', 'aa', '1234', 10000),
-       (now(), now(), 'ROLE_USER', 'bb@naver.com', 'OFFLINE', 'bb', 'bb', '1234', 10000),
-       (now(), now(), 'ROLE_USER', 'cc@naver.com', 'OFFLINE', 'cc', 'cc', '1234', 10000),
-       (now(), now(), 'ROLE_USER', 'dd@naver.com', 'OFFLINE', 'dd', 'dd', '1234', 10000),
-       (now(), now(), 'ROLE_ADMIN', 'ee@naver.com', 'OFFLINE', 'ee', 'ee', '1234', 10000);
+values (now(), now(), 'ROLE_USER', 'aa@naver.com', 'OFFLINE', 'aa', 'aa', '$2a$10$CeM8t1vXAQODtBKmfI7MA.CnhtBM06F3ejq2DBbDMnGUCJ/RC/KCS', 10000),
+       (now(), now(), 'ROLE_USER', 'bb@naver.com', 'OFFLINE', 'bb', 'bb', '$2a$10$CeM8t1vXAQODtBKmfI7MA.CnhtBM06F3ejq2DBbDMnGUCJ/RC/KCS', 10000),
+       (now(), now(), 'ROLE_USER', 'cc@naver.com', 'OFFLINE', 'cc', 'cc', '$2a$10$CeM8t1vXAQODtBKmfI7MA.CnhtBM06F3ejq2DBbDMnGUCJ/RC/KCS', 10000),
+       (now(), now(), 'ROLE_USER', 'dd@naver.com', 'OFFLINE', 'dd', 'dd', '$2a$10$CeM8t1vXAQODtBKmfI7MA.CnhtBM06F3ejq2DBbDMnGUCJ/RC/KCS', 10000),
+       (now(), now(), 'ROLE_ADMIN', 'ee@naver.com', 'OFFLINE', 'ee', 'ee', '$2a$10$CeM8t1vXAQODtBKmfI7MA.CnhtBM06F3ejq2DBbDMnGUCJ/RC/KCS', 10000);
 
 insert into friend_request (created_date, last_modified_date, friend_request_status, from_member_id, to_member_id)
 values (now(), now(), 'PROCESS', 1, 2),
