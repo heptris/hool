163bfd074422d7f9c50407a1ad71e374cc713467
YanZisuka <hanndrednine@gmail.com>
2022-08-11 02:12:33 +0900

    :white_check_mark: [TEST] 소셜 페이지 테스트 업데이트

--- frontend/tests/Social.test.tsx
+++ frontend/tests/Social.test.tsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render } from "@testing-library/react";
 import SocialPage from "../src/pages/social";
 
 describe("<SocialPage />", () => {
+  const queryClient = new QueryClient();
   it("matches snapshot", () => {
-    const utils = render(<SocialPage />);
+    const utils = render(
+      <QueryClientProvider client={queryClient}>
+        <SocialPage />
+      </QueryClientProvider>
+    );
     expect(utils.container).toMatchSnapshot();
   });
 
   it("shows the props correctly", () => {
-    const utils = render(<SocialPage />);
+    const utils = render(
+      <QueryClientProvider client={queryClient}>
+        <SocialPage />
+      </QueryClientProvider>
+    );
     utils.getByText("친구찾기");
     utils.getByText("친구와 함께하면 즐거움이 두배에요.");
     utils.getByText("내친구");
