98d20da152b1d89da3807d4ee5d42781b8df4dc7
HojinAn <asdv94@naver.com>
2022-08-15 18:01:33 +0900

    :truck: [MOVE] 미팅헤더 폴더 위치 변경

--- frontend/src/components/meeting/index.ts
+++ frontend/src/components/meeting/index.ts
@@ -1,3 +1,3 @@
 export { default as MeetingList } from "./list/MeetingList";
-export { default as MeetingHeader } from "./MeetingHeader";
+export { default as MeetingHeader } from "./list/MeetingHeader";
 export { default as MeetingModal } from "./modal/MeetingModal";
