17f2462503b06060356fbc590f0e4f267a6c32d8
HojinAn <asdv94@naver.com>
2022-08-17 15:04:27 +0900

    :bug: [FIX] 빌드 시 새로고침 때 로그인 되지 않는 오류 해결

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,22 +1,21 @@
 import axios from "axios";
 
-import { HOOL_API_ENDPOINT } from "constant";
+import { HOOL_API_ENDPOINT, USER_AUTH_KEY } from "constant";
 
 const apiInstance = (baseURL = HOOL_API_ENDPOINT) => {
   const api = axios.create({
     baseURL,
     headers: { "Content-Type": "application/json" },
   });
+  api.interceptors.request.use((config) => {
+    const accessToken = localStorage.getItem(USER_AUTH_KEY.ACCESS_TOKEN);
+    config.headers!.Authorization = `Bearer ${accessToken}`;
+    return config;
+  });
   return api;
 };
 
 const api = apiInstance();
-
-const setApiHeaders = (accessToken?: string) => {
-  api.defaults.headers.common["Authorization"] = accessToken
-    ? `Bearer ${accessToken}`
-    : false;
-};
 
 const getRequest = async (path: string, obj?: object) => {
   return await api.get(path, obj).then((res) => res.data);
@@ -31,11 +30,4 @@ const deleteRequest = async (path: string, data?: object) => {
   return await api.delete(path, { data }).then((res) => res.data);
 };
 
-export {
-  apiInstance,
-  getRequest,
-  postRequest,
-  putRequest,
-  deleteRequest,
-  setApiHeaders,
-};
+export { apiInstance, getRequest, postRequest, putRequest, deleteRequest };
